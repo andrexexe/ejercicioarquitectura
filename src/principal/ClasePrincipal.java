8fbe96bca83877b80bfef5c75f4d8b59d84e8588
andrexexe <andresdmendez@gmail.com>
2023-02-09 20:33:31 -0500

    creacion de rama

--- src/principal/ClasePrincipal.java
+++ src/principal/ClasePrincipal.java
@@ -3,5 +3,7 @@ package principal;
 public class ClasePrincipal {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("hhs");
+        System.out.println("CLase PRo");
     }
 }
